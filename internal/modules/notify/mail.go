132a251fb6b9062dd4cf861d3695d5231d876966
jin hua <zjin_hua@126.com>
2022-03-15 17:20:03 +0800

    Update mail.go

--- internal/modules/notify/mail.go
+++ internal/modules/notify/mail.go
@@ -52,7 +52,7 @@ func (mail *Mail) send(mailSetting models.Mail, toUsers []string, msg Message) {
 	gomailMessage := gomail.NewMessage()
 	gomailMessage.SetHeader("From", mailSetting.User)
 	gomailMessage.SetHeader("To", toUsers...)
-	gomailMessage.SetHeader("Subject", "gocron-定时任务通知")
+	gomailMessage.SetHeader("Subject", "gocron-定时任务通知预警")
 	gomailMessage.SetBody("text/html", body)
 	mailer := gomail.NewDialer(mailSetting.Host, mailSetting.Port,
 		mailSetting.User, mailSetting.Password)
